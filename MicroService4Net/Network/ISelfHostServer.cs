2a4dee4dd1972a9c8d644b32b1d990e314e47b08
Andreas Mersch <andreas.mersch@smp-tubes.com>
2018-02-07 13:16:12 +0100

    Filename error.

---
+++ MicroService4Net/Network/ISelfHostServer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Web.Http;
+
+namespace MicroService4Net.Network
+{
+    public interface ISelfHostServer
+    {
+        void Connect(Action<HttpConfiguration> configure, bool useCors);
+        void Dispose();
+    }
+}
