a6eeb1c4964727ac4a4996971d0e2ea066e0e46b
Andreas Mersch <andreas.mersch@smp-tubes.com>
2018-02-07 11:31:36 +0100

    Nugat packages updated.

--- MicroService4Net.Ninject.Example/Controllers/ExampleController.cs
+++ MicroService4Net.Ninject.Example/Controllers/ExampleController.cs
@@ -1,12 +1,25 @@
 ﻿using System.Web.Http;
+using Ninject;
+using Ninject.Extensions.Logging;
 
 namespace MicroService4Net.Example.Controllers
 {
+    [RoutePrefix("api/v1/Example")]
     public class ExampleController : ApiController
     {
-        [Route("Example")]
+        ILogger m_logger;
+
+        public ExampleController(ILogger logger)
+            : base()
+        {
+            this.m_logger = logger;
+            this.m_logger?.Debug("Ctor");
+        }
+
+        [Route("")]
         public string GetExample()
         {
+            this.m_logger?.Debug("GetExample");
             return "Example";
         }
     }
